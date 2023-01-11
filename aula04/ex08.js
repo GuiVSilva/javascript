e6c787a1c0e9c117a884353467359d2d17d270f4
Guilherme Vieira <vieirasilvaguilherme70@gmail.com>
2023-01-10 23:24:37 -0300

    exercicio finalizado

---
+++ aula04/ex08.js
@@ -0,0 +1,6 @@
+var velocidade = 12
+console.log(`A velocidade do seu carro é ${velocidade} KM/h`)
+if(velocidade >= 60) { //condição simples
+  console.log('voce ultrapassou a velocidade permitida, MULTADO')
+}
+console.log('Dirija com cuidado')
